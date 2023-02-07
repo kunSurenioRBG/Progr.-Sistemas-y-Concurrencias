fe7908a53aa76b95d894ec42f5c0a9de77b69a15
Santiago Ponce Arrocha <poncearochasanti@hotmail.com>
2023-02-07 19:39:44 +0100

    first upload

---
+++ tema2/holaMundo.c
@@ -0,0 +1,13 @@
+/*
+Programa: holaMundo.c
+Descripcion: Programa que muestra "Hello World"
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World \n");
+    printf("Ana juega a los sims \n");
+    return 0;
+}
