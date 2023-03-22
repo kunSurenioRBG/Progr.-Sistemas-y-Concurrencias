4bf40a15e85b9d38a374b4c6ef706ebefd65b847
kunSurenioRBG <poncerochasanti@hotmail.com>
2023-03-22 12:58:02 +0100

    ejercicio_indeterminismo

--- tema5/ejercicios_propios/principal_1_4.java
+++ tema5/ejercicios_propios/principal_1_4.java
@@ -1,6 +1,40 @@
 package tema5.ejercicios_propios;
-// indeterminismo
+// Indeterminismo: cuando 2 o mas hilos escriben a la vez en una misma variable compartida, el valor de esta variable es indeterminado.
 
-public class principal_1_4 {
-    
+// Seccion Critica: parte de codigo donde sabemos que se va a producir indeterminismo.
+
+public class principal_1_4 extends Thread {
+    private static int cont = 0;
+
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            cont++;
+        }
+    }
+
+    public static void main(String[] args) {
+        principal_1_4 vec[] = new principal_1_4[1000];
+
+        // inicializa el vector en la posicion i
+        for (int i = 0; i < vec.length; i++) {
+            vec[i] = new principal_1_4();
+            vec[i].start();
+        }
+
+        // RECORDATORIO
+        // JOIN hara que el hilo principal se espera a que los hilos de los subprocesos
+        // hayan terminado de ejecutarse.
+        try {
+            for (int i = 0; i < vec.length; i++) {
+                vec[i].join();
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        System.out.println(cont);
+
+    }
 }
+
+// Muy rara vez va a salir 1.000.000, debido al problema del indeterminismo.
---
+++ tema5/ejercicios_propios/principal_1_5.java
@@ -0,0 +1,56 @@
+package tema5.ejercicios_propios;
+
+import java.util.Random;
+
+//Realizar la multiplicacion de todos los elementos del un vector por 10
+public class principal_1_5 extends Thread {
+    // Cuando trabajamos con hilos, recomendamos usar 'static', para que varios
+    // hilos puedan acceder al mismo vector.
+    private static int tam = 8;
+    private static int[] vec = new int[tam];
+
+    private int init, fin;
+
+    public principal_1_5(int init, int fin) {
+        this.init = init;
+        this.fin = fin;
+    }
+
+    public void run() {
+        for (int i = init; i < fin; i++) { // h1: (init = 0 - fin = 4) / h2: (init = 4 - fin = 8)
+            vec[i] *= 10;
+        }
+        // si lo dejamos asi, el hilo 1, h1, multiplicara todos los valores del vector
+        // por 10, pero luego el hilo 2, h2, lo hara tambien
+        // por tanto, tenemos un problema de indeterminismo.
+        //
+        // por ello, inicializamos el constructor y dos variable 'init' y 'fin' y en los
+        // hilos marcamos el rango de que mitad cogera cada hilo para multiplicar.
+    }
+
+    public static void main(String[] args) {
+        // valor aleatorio
+        Random random = new Random(System.nanoTime());
+
+        for (int i = 0; i < vec.length; i++) {
+            vec[i] = random.nextInt(10); // numero aleatorio entre 0 y (10, en nuestro caso)
+        }
+
+        principal_1_5 h1 = new principal_1_5(0,4);
+        principal_1_5 h2 = new principal_1_5(4,8);
+
+        h1.start();
+        h2.start();
+
+        // no va a continuar al hilo principal hasta acabar los hilos de los subprocesos
+        try {
+            h1.join();
+            h2.join();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        for (int i = 0; i < vec.length; i++) {
+            System.out.print(vec[i] + " ");
+        }
+    }
+}
