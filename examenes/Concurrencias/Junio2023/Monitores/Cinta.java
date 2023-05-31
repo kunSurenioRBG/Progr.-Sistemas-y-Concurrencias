1ffaf9c731dc429d2b9b5318d2af63f5073b3226
kunSurenio <poncearochasanti@hotmail.com>
2023-05-31 23:37:03 +0200

    Junio2023 - Monitores actualizado

--- examenes/Concurrencias/Junio2023/Monitores/Cinta.java
+++ examenes/Concurrencias/Junio2023/Monitores/Cinta.java
@@ -6,52 +6,53 @@ public class Cinta {
 	private boolean turistaRetira = false;
 
 	private int maletasPrimera, maletasTurista, primeraEsperando;
-	
+
 	public Cinta() {
 		maletasPrimera = 0;
 		maletasTurista = 0;
 		primeraEsperando = 0;
 	}
-	
+
 	public synchronized void poner(boolean primeraClase) throws InterruptedException {
 		if (primeraClase == true) {
-			//primera clase
+			// primera clase
 			maletasPrimera++;
 			primeraEsperando++;
-			System.out.println("Generador pone maleta de primera clase. Total primera: "+maletasPrimera+" Total turista: "+maletasTurista);
+			System.out.println("Generador pone maleta de primera clase. Total primera: " + maletasPrimera
+					+ " Total turista: " + maletasTurista);
 			primeraVaRetirar = true;
 			notifyAll();
 		} else {
-			//turista
+			// turista
 			maletasTurista++;
-			System.out.println("Generador pone maleta de clase turista. Total primera: "+maletasPrimera+" Total turista: "+maletasTurista);
+			System.out.println("Generador pone maleta de clase turista. Total primera: " + maletasPrimera
+					+ " Total turista: " + maletasTurista);
 			turistaVaRetirar = true;
 			notifyAll();
 		}
 	}
-	
 
 	public synchronized void qRetirarPrimera(int pasajeroId) throws InterruptedException {
 		while (!primeraVaRetirar) {
 			wait();
 		}
-		System.out.println("***Pas. primera "+pasajeroId+" quiere maleta***");
-		System.out.println("***Pas. primera "+pasajeroId+" entra a la cinta***");
+		System.out.println("***Pas. primera " + pasajeroId + " quiere maleta***");
+		System.out.println("***Pas. primera " + pasajeroId + " entra a la cinta***");
 		primeraRetira = true;
 		primeraVaRetirar = false;
 		notifyAll();
 	}
-	
+
 	public synchronized void qRetirarTurista(int pasajeroId) throws InterruptedException {
 		while (!turistaVaRetirar || primeraEsperando > 0) {
 			wait();
 		}
-		System.out.println("Pas. turista "+pasajeroId+" entra a la cinta");
+		System.out.println("Pas. turista " + pasajeroId + " entra a la cinta");
 		turistaRetira = true;
 		turistaVaRetirar = false;
 		notifyAll();
 	}
-	
+
 	public synchronized void fRetirarPrimera(int pasajeroId) throws InterruptedException {
 		while (!primeraRetira) {
 			wait();
@@ -60,8 +61,9 @@ public class Cinta {
 		primeraEsperando--;
 		primeraRetira = false;
 		notifyAll();
-		System.out.println("***Pas. primera "+pasajeroId+" coge su maleta***");
+		System.out.println("***Pas. primera " + pasajeroId + " coge su maleta***");
 	}
+
 	public synchronized void fRetirarTurista(int pasajeroId) throws InterruptedException {
 		while (!turistaRetira) {
 			wait();
@@ -69,7 +71,7 @@ public class Cinta {
 		maletasTurista--;
 		turistaRetira = false;
 		notifyAll();
-		System.out.println("Pas. turista "+pasajeroId+" coge su maleta");
+		System.out.println("Pas. turista " + pasajeroId + " coge su maleta");
 	}
 
 }
